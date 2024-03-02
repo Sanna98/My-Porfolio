d33dcc5930c40c7df618e0d003a0db0490fa2cdf
Sanna98 <samithaherath027@gmail.com>
2024-03-02 10:22:01 -0800

    Home Page Done

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer","Backend Developer","Fullstack Developer","Mobile App Developer","Cloud Engineer","Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
